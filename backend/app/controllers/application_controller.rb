99c0426f79b95e9f0be1bbff7bded5cb5cbed9ba
kamausuzan197 <kamaususan197@gmail.com>
2022-09-03 20:08:04 +0300

    create the application file

---
+++ backend/app/controllers/application_controller.rb
@@ -0,0 +1,27 @@
+class ApplicationController < Sinatra::Base
+    set :default_content_type, "application/json"
+    get "/lists" do
+        lists = List.all 
+        lists.to_json 
+    end
+
+    post "/lists" do 
+         list = List.create(
+           title: params[:title],
+           status: params[:status] 
+         )
+         list.to_json
+    end
+
+    patch "/lists/:id" do
+        list = List.find(params[:id])
+        list.update(title: params[:title])
+        list.to_json
+    end
+
+    delete "/list/:id" do
+        list = List.find(params[:id])
+        list.destroy
+        list.to_json
+    end
+end
--- frontend/src/components/List.js
+++ frontend/src/components/List.js
@@ -7,7 +7,7 @@ function List() {
       <form>
         <label>Title</label>
         <br />
-        <input type="text" placeholder="My_text_here" />
+        <input className='input_type' type="text" placeholder="My_text_here" />
         <button type="submit" placeholder="submit">Submit</button>
       </form>
     </div>
