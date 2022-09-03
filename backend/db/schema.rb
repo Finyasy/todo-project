193f2263fcbdb61dee8c508c23426de1ff80ddda
mmulahvictor <mmulahvictor@gmail.com>
2022-09-03 16:30:02 +0300

    deleted some files

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,14 +10,4 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_08_28_223805) do
 
-  create_table "create_movies", force: :cascade do |t|
-    t.string "title"
-    t.integer "release_date"
-    t.string "director"
-    t.string "lead"
-    t.boolean "in_theaters"
-  end
-
-end
